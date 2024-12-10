2a9f5e70911be7b18e5fa77cb04b83f78d0cb128
jrhawkinsPrime <162509409+jrhawkinsPrime@users.noreply.github.com>
2024-12-10 13:27:32 -0800

    Update tFormItems.lua
    added #6 - #18

--- tFormItems.lua
+++ tFormItems.lua
@@ -6,6 +6,19 @@
 -- Muradin's Favor [3]
 -- Iron Boot Flask [4]
 -- Burgy Blackheart's Handsome Hat [5]
+-- Gnomeregan Pride [6]
+-- Time-Lost Figurine [7]
+-- Orb of the Sin'dorei [8]
+-- Orb of Deception [9]
+-- Vial of Green Goo [10]
+-- Savory Deviate Delight [11]
+-- Kalytha's Haunted Locket [12]
+-- Carved Ogre Idol [13]
+-- Gordok Ogre Suit [14]
+-- Magical Ogre Idol [15]
+-- Mr. Smite's Brass Compass [16]
+-- Krastinov's Bag of Horrors [17]
+-- Potion of Illusion [18]
 
 local transformItems = {
     [1] = {
@@ -81,6 +94,136 @@ local transformItems = {
         expansion = "Cataclysm",
         message = "Arrr! Embracing my inner pirate ghost!",
         usableInCombat = nil -- Unknown, left blank for now
+    },
+    [6] = {
+        id = 54651,
+        source = "Quest reward from 'Liberation of Gnomeregan'",
+        requirements = {
+            "Complete the quest 'Liberation of Gnomeregan'"
+        },
+        location = "Gnomeregan",
+        message = "For Gnomeregan!",
+        usableInCombat = nil -- Unknown, left blank for now
+    },
+    [7] = {
+        id = 32782,
+        source = "Drop from Terokk",
+        requirements = {
+            "Defeat Terokk in Skettis, Terokkar Forest"
+        },
+        location = "Skettis, Terokkar Forest",
+        message = "Embracing the form of a Skettis arakkoa!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [8] = {
+        id = 35275,
+        source = "Dropped in Heroic Magisters' Terrace",
+        requirements = {
+            "Defeat bosses in Heroic Magisters' Terrace"
+        },
+        location = "Magisters' Terrace, Isle of Quel'Danas",
+        message = "Embracing the elegance of the Sin'dorei!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [9] = {
+        id = 1973,
+        source = "World Drop",
+        requirements = {},
+        location = "Various high-level pre-Burning Crusade instances",
+        message = "Assuming the guise of the opposing faction!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [10] = {
+        id = 151265,
+        source = "Drop from Endgineer Omegaplugg",
+        requirements = {
+            "Defeat Endgineer Omegaplugg in Gnomeregan"
+        },
+        location = "Gnomeregan",
+        message = "Transforming into a leper gnome!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [11] = {
+        id = 6657,
+        source = "Crafted via Cooking",
+        requirements = {
+            "Cooking skill of 85 or higher",
+            "Recipe: Savory Deviate Delight"
+        },
+        location = "Crafted; recipe drops in The Barrens",
+        message = "Transforming into a pirate or ninja!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [12] = {
+        id = 68806,
+        source = "Vendor: Tex Vortacoil",
+        requirements = {
+            "5,000 gold"
+        },
+        location = "Bear's Head, Azshara",
+        message = "Inviting the spirit of Kalytha to overtake you!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [13] = {
+        id = 49704,
+        source = "World of Warcraft Trading Card Game UDE Points System",
+        requirements = {
+            "Redeem 25,000 UDE points"
+        },
+        location = "Unavailable through in-game means; previously via TCG",
+        message = "Transforming into a hulking red ogre!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [14] = {
+        id = 18258,
+        source = "Crafted by Knot Thimblejack",
+        requirements = {
+            "Materials: 8x Rugged Leather, 4x Bolt of Runecloth, 1x Ogre Tannin, 2x Rune Thread"
+        },
+        location = "Dire Maul North",
+        message = "Disguising yourself as a Gordok ogre!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [15] = {
+        id = 72159,
+        source = "World of Warcraft Trading Card Game",
+        requirements = {
+            "Obtain the 'Magical Ogre Idol' loot card from the Crown of the Heavens expansion"
+        },
+        location = "Redeem at Landro Longshot in Booty Bay",
+        message = "Transforming into a powerful red ogre mage!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [16] = {
+        id = 86568,
+        source = "Drop from Yorik Sharpeye",
+        requirements = {
+            "Defeat Yorik Sharpeye in the Vale of Eternal Blossoms"
+        },
+        location = "Vale of Eternal Blossoms",
+        message = "Releasing the memories of a long-lost first mate!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [17] = {
+        id = 88566,
+        source = "Drop from Doctor Theolen Krastinov",
+        requirements = {
+            "Defeat Doctor Theolen Krastinov in Heroic Scholomance"
+        },
+        location = "Scholomance",
+        message = "Performing some quick surgery!",
+        usableInCombat = nil -- Information not specified; field left blank
+    },
+    [18] = {
+        id = 58489,
+        source = "Alchemy",
+        requirements = {
+            "Alchemy skill level 460",
+            "Materials: 3x Volatile Life, 1x Azshara's Veil, 1x Crystal Vial"
+        },
+        location = "Crafted by players with Alchemy",
+        message = "Creating a fleeting illusion!",
+        usableInCombat = nil -- Information not specified; field left blank
     }
 }
 
